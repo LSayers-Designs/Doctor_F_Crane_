f1d208e7263a3268edb9f9244aa2e94d73193cb0
LSayers-Designs <58526383+LSayers-Designs@users.noreply.github.com>
2020-05-04 15:14:38 -0400

    minor adjustments

--- doctorgui.py
+++ doctorgui.py
@@ -1,4 +1,12 @@
-"""Random Winner Generator"""
+"""
+Program: doctorGUI.py
+Author: Letisha Rahming
+Date Created
+Date Completed
+Last Updated
+Description: 
+
+"""
 
 from breezypythongui import EasyFrame
 import random
@@ -8,15 +16,15 @@ hedges = ("Please tell me more.", "Many of my patients tell me the same thing", 
 qualifiers = ("Why do you say that ", "You seem to think that ", "Can you explain why ")
 replacements = { "I": "you", "I am": "you are","my": "your", "I'm": "you're", "me":"you", "mine":"yours", "I've": "You've" "I've"}
 
-class doctorGUI(EasyFrame):
+class doctorCrane(EasyFrame):
 	def __init__(self):
-		EasyFrame.__init__(self, title = "Eliza 1967")
-		self.addLabel(text = "Good Morning.  Hope you are well today", row = 0, column = 0)
-		self.addLabel(text = "What can I do for you?", row = 1, column = 0)
+		EasyFrame.__init__(self, title = "Doctor F. Crane")
+		self.addLabel(text = "Good Morning.  Hope you are well.", row = 0, column = 0, sticky = "NSEW")
+		self.addLabel(text = "This is Dr. Fraiser Crane, how can I help you today?", row = 1, column = 0, sticky = "NSEW")
 		self.userText = self.addTextField(text = "", row = 2, column = 0, sticky = "NSEW")
 		#Bind the userText field to the enter key
 		self.userText.bind("<Return>", lambda event: self.reply())
-		self.responseLabel = self.addLabel(text = "", row = 3, column = 3)
+		self.responseLabel = self.addLabel(text = "", row = 3, column = 0, sticky = "NSEW")
 		self.addButton(text = "Submit", row = 4, column = 0, command = self.reply)
 
 	def reply(self):
@@ -34,5 +42,5 @@ def changePerson(sentence):
 		replywords.append(replacements.get(word, word))
 	return " ".join(replywords)
 def main():
-	doctorGUI().mainloop()
+	doctorCrane().mainloop()
 main()
